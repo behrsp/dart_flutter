bcdcddc7ab351dfe8a426e0842f42cde236d16e1
Robson <90429740+behrsp@users.noreply.github.com>
2025-06-27 07:28:18 -0300

    Add files via upload

--- listas/listas.dart
+++ listas/listas.dart
@@ -2,5 +2,10 @@ import 'dart:io';
 
 main(){
 
+    var names = ['Robson','Behr','Mary','Gui'];
+    names.add('mae'); // adicionar item a lista
+    names.remove('Behr'); // remover
+    names.remove(2); // remove por indice
+    print(names);
 
 }
